a418c1af1a29de9dbb31ccff11651e1687dddbfe
Raj Kumar <bornagain.raj7@gmail.com>
2020-04-15 22:23:06 +0530

    steps

--- exercises/try.js
+++ exercises/try.js
@@ -1,7 +1,26 @@
-// var a = '20';
-// var b = 10;
-// console.log(a+b);
+var a = '20';
+var b = '10';
+console.log(a+b); // o/p: 2010
+console.log(a-b); // o/p: 10
 
 
-const word = 'Hi There!!!';
-console.log(word.replace(/[^\w]/g, ''));
+// const word = 'Hi There!!!';
+// console.log(word.replace(/[^\w]/g, ''));
+
+// function steps(n) {
+//     for (let row = 0; row < n; row++) {
+//         let stair = '';
+//         for (let col = 0; col < n; col++) {
+//             if (col <= row) {
+//                 stair += '#';
+//             } else {
+//                 stair += ' ';
+//             }
+//         }
+
+//         console.log(stair);
+//     }
+// }
+
+// steps(3);
+// steps(5);
